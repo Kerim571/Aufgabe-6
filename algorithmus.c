cd4115602e9bb116c19965f119ccabfd191a9a77
kerim <kerim.altindal@stud.thga.de>
2022-09-06 15:23:27 +0200

    verschachtelte for-Anweisung erstellt, Funktion fehlerhaft

--- algorithmus.c
+++ algorithmus.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
+#define SIZE 10
 
 int main(void){
 
-	printf( "%s%13s%17s\n", "Element", "Value", "Histogramm");	
+	int n[SIZE] = {1,2,3,4,5,12,7,8,9,10};
+        size_t i;
+        int j;
+
+	printf( "%s%13s%17s\n", "Element", "Value", "Histogramm");
+
+        for ( i=0; i<SIZE; ++i){
+		printf("%7u%13d         ", i, n[i]);
+
+		for( j=1; j=i; ++j){
+			printf("%c", '*');
+		}
+
+	}
+}
